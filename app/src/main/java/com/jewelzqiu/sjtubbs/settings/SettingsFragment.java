7814f027f64bf9319f9463fbc0a3f2ebdc0cd928
jewelzqiu <jewelz.q.915@gmail.com>
2014-06-11 16:56:57 +0800

    Add about page.

--- app/src/main/java/com/jewelzqiu/sjtubbs/settings/SettingsFragment.java
+++ app/src/main/java/com/jewelzqiu/sjtubbs/settings/SettingsFragment.java
@@ -1,5 +1,11 @@
 package com.jewelzqiu.sjtubbs.settings;
 
+import com.jewelzqiu.sjtubbs.R;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 /**
@@ -7,4 +13,19 @@ import android.preference.PreferenceFragment;
  */
 public class SettingsFragment extends PreferenceFragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_settings);
+        Preference preference = findPreference(getString(R.string.key_github));
+        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        (String) preference.getSummary()));
+                startActivity(intent);
+                return true;
+            }
+        });
+    }
 }
